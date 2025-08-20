137702444dc814dff5949dc30f29acbada0f66a9
Everton Nascimento <124212111+Evy8882@users.noreply.github.com>
2025-08-20 12:26:31 -0300

    adiciona dependencia openai e chave de api

---
+++ mentoris-app/utils/AI.ts
@@ -0,0 +1,26 @@
+"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"
+import OpenAI from 'openai';
+const openai = new OpenAI({
+  baseURL: "https://openrouter.ai/api/v1",
+  apiKey: "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",
+  defaultHeaders: {
+    "HTTP-Referer": "<YOUR_SITE_URL>", // Optional. Site URL for rankings on openrouter.ai.
+    "X-Title": "<YOUR_SITE_NAME>", // Optional. Site title for rankings on openrouter.ai.
+  },
+});
+async function main() {
+  const completion = await openai.chat.completions.create({
+    model: "deepseek/deepseek-r1:free",
+    messages: [
+      {
+        "role": "user",
+        "content": "What is the meaning of life?"
+      }
+    ],
+    
+  });
+
+  console.log(completion.choices[0].message);
+}
+
+main();
