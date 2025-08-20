82de9cac51cb1aaebca5a9c260609c36a5b15ca9
Everton Nascimento <124212111+Evy8882@users.noreply.github.com>
2025-08-20 17:08:34 -0300

    Adiciona backend e conexão API

--- mentoris-app/app/index.tsx
+++ mentoris-app/app/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View } from "react-native";
+import askAI from "@/utils/AI";
 
 export default function Index() {
   return (
@@ -9,7 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit index.tsx</Text>
+      <Text>{askAI()}</Text>
     </View>
   );
 }
