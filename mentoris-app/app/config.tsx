d45be04fb65f0cabfe0fdfda9a59bf2451fd353b
Matheus Serra <ggmat304@gmail.com>
2025-09-01 22:39:37 -0300

    Criação da pagina de componentes de estudos em matemática

--- mentoris-app/app/config.tsx
+++ mentoris-app/app/config.tsx
@@ -1,10 +1,14 @@
 import TabNavigator from "@/component/TabNavigator"
 import { View } from "react-native"
+import Matematica from "./materias/matematica"
 
 export default function Config(){
     return (
         <View>
-            <TabNavigator></TabNavigator>
+            
+            <TabNavigator>
+
+            </TabNavigator>
         </View>
     )
 }
